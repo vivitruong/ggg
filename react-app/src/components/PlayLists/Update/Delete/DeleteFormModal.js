07c5be80771aa44a0e4c16c8309bc47368f12dcb
vivitruong <vivitruong1506@gmail.com>
2023-10-04 17:27:12 -0500

    still cant figure out how

--- react-app/src/components/PlayLists/Update/Delete/DeleteFormModal.js
+++ react-app/src/components/PlayLists/Update/Delete/DeleteFormModal.js
@@ -1,24 +1,42 @@
 import { useEffect } from "react";
 import DeletePlaylistForm from "./DeletePlaylistForm";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { useModal } from "../../../../context/Modal";
+import { useHistory } from "react-router-dom";
+import * as playlistActions from '../../../../store/playlist'
 
-const DeleteFormModal = ({ playlist, closeDropdown, openModal, closeModal }) => {
-    
-    const onClick = (e) => {
-        e.stopPropagation();
-        openModal(
-            <DeletePlaylistForm playlist={playlist} onClose={() => closeModal()} />
-        );
-        closeDropdown();
-    };
 
-    useEffect(() => {
-        document.addEventListener('click', e => {
-            closeDropdown();
-        });
-    }, [closeDropdown]);
+const DeleteFormModal = ({ id }) => {
+    const { closeModal } = useModal();
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch(playlistActions.deletePlaylist(id))
+          .then(() => {
+            closeModal();
+            history.push("/playlists");
+          })
+      };
+
+
+    // const onClick = (e) => {
+    //     e.stopPropagation();
+    //     openModal(
+    //         <DeletePlaylistForm playlist={playlist} onClose={() => closeModal()} />
+    //     );
+    //     closeDropdown();
+    // };
+
+    // useEffect(() => {
+    //     document.addEventListener('click', e => {
+    //         closeDropdown();
+    //     });
+    // }, [closeDropdown]);
 
     return (
-        <button onClick={onClick} className='deleteform-btn'style={{width:"100%", textAlign:"left"}}>
+        <button onClick={handleSubmit} className='deleteform-btn'style={{width:"100%", textAlign:"left"}}>
             <span className="deleteform-span">Delete</span>
         </button>
     );
--- react-app/src/pages/CreatePlayList.jsx
+++ react-app/src/pages/CreatePlayList.jsx
@@ -4,17 +4,27 @@ import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Modal from '../components/Modal'
 // import Playlist from '../components/PlayLists/Playlist'
+import OpenModalButton from '../components/OpenModalButton';
+import DeleteFormModal from '../components/PlayLists/Update/Delete/DeleteFormModal';
+import * as playlistActions from '../store/playlist';
 
 
 const CreatePlayList = () => {
   const [showModal, setShowModal] = useState(false);
   const user = useSelector(state => state.session.user);
+  // const playlists = useSelector((state) => Object.values(state.playlist));
+
   return (
     <>
     {user !== null &&  <div onClick={() => setShowModal(true)} className='app-left-make-playlistcomp'>
 
     <span style={{ cursor: 'pointer', color: 'black', transition: 'color 0.3s' }} onMouseEnter={(e) => e.target.style.color = 'white'} onMouseLeave={(e) => e.target.style.color = 'black'}>Create Your New Playlist</span>
-
+    {/* <div className="button-container">
+                    <OpenModalButton
+                      modalComponent={<DeleteFormModal id={id} />}
+                      buttonText="Delete"
+                    />
+                  </div> */}
 </div>
 
 }
@@ -23,6 +33,7 @@ const CreatePlayList = () => {
             <PlaylistForm onClose={() => setShowModal(false)}/>
         </Modal>
     )}
+
 {/* <div className='app-left-playlistarea'>
    <Playlist/>
 </div> */}
