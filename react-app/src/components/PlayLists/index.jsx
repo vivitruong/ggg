4c7ec7f0d54264b1d87780de33cd6973fcf5e3ff
vivitruong <vivitruong1506@gmail.com>
2023-10-08 23:09:59 -0500

    final change

--- react-app/src/components/PlayLists/index.jsx
+++ react-app/src/components/PlayLists/index.jsx
@@ -38,8 +38,9 @@ const PlayLists = () => {
   <span style={{ fontSize: '20px', paddingBottom:'10px', fontWeight: '600' }}>
     My Playlists <img src='https://win98icons.alexmeub.com/icons/png/cd_audio_cd-1.png' alt="CD Icon" />
   </span>
+
   {renderList.map((item, index) => (
-    <div key={index} style={{ padding: '6px 0', fontWeight:'500', fontSize:'13px' }}>
+    <div key={index} style={{ padding: '6px 0', fontWeight:'600', fontSize:'12.5px' }}>
 
       {item}
     </div>
